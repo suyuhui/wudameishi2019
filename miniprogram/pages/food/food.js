7d4fa3a49043f42f50745d4473a0d225a31949c1
unknown <1216048351@qq.com>
2019-05-10 22:07:38 +0800

    add food details

---
+++ miniprogram/pages/food/food.js
@@ -0,0 +1,70 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    img_src:"http://i4.piimg.com/601998/9ce47f2f19d7717d.jpg",
+    name:"传世排骨汤饭",
+    restaurant_name:"信息学部一食堂",
+    port:"2号档口",
+    evaluation:"4.5",
+    detail:"传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭传世排骨饭"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
